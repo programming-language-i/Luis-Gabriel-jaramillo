76fd8396896ad19cecd377d89cb8cf8d4be6576b
luis <luisgb.jaramillo@gmail.com>
2026-09-22 15:23:30 -0500

    agrego estos archivos

---
+++ calcu.py
@@ -0,0 +1,22 @@
+def suma(valor1, valor2):
+    return valor1 + valor2
+
+
+def resta(valor1, valor2):
+    return valor1 - valor2
+
+
+def multi(valor1, valor2):
+    return valor1 * valor2
+
+
+resultado_suma = suma(5, 2)
+
+resultado_resta = resta(5, 2)
+
+resultado_multi = multi(5, 2)
+
+
+print(f"resultado_suma: {resultado_suma}")
+print(f"resultado_resta: {resultado_resta}")
+print(f"resultado_multi: {resultado_multi}")
---
+++ ejerc-01.py
@@ -0,0 +1,20 @@
+import threading
+import time
+
+
+def imprimir_mensaje():
+    for i in range(5):
+        print("Hello")
+        time.sleep(1)
+
+
+def main():
+    thread = threading.Thread(target=imprimir_mensaje)
+    thread.start()
+    thread.join()
+
+    print("finalizado")
+
+
+if __name__ == "__main__":
+    main()
